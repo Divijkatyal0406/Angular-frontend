7a3268a99cc2da81e9e244f88d64e7f71b9ae6a8
Divij Katyal <divijkatyal@gmail.com>
2022-11-19 16:16:36 +0530

    new-employee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 const routes: Routes = [
   {path:'employee',component:EmployeeListComponent},
+  {path:'create-employee',component:CreateEmployeeComponent},
   {path:'',redirectTo:'employee',pathMatch:'full'}
 ];
 
